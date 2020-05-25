cd31ab13490d798ace2884e7e066b720d7bd8cae
brokoli <bocalist@gmail.com>
2020-05-25 22:56:17 +0900

    structure of router and controllers

--- server.ts
+++ server.ts
@@ -1,12 +1,10 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
 
 const port = 5000;
 const app = new Application();
-const router = new Router();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-router.get("/api/v1/products");
 
 await app.listen({ port });
