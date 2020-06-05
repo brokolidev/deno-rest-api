aee3af1d08a9007cefb7f42fbb9d34159b4116b2
brokoli <bocalist@gmail.com>
2020-06-05 15:54:57 +0900

    install denon

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./routes.ts";
 
-const port = 5000;
+const port = Deno.env.get("PORT") || 5000;
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port });
+console.log(`Server running on port ${port}`);
+
+await app.listen({ port: +port });
